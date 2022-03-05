6e347875b03795fc04c5dce27e35146e3cd956ae
Vivien Talos <vivientalos29@gmail.com>
2022-03-05 16:55:02 +0100

    Get some Chuck Norris jokes

--- server.ts
+++ server.ts
@@ -1,11 +1,9 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
-import * as cors from 'cors';
 
 const app = express();
 const port = 3000;
 
-app.use(cors());
 app.use(express.static('public'));
 
 app.listen(port, () => {
